3ba75f6069ce22577b37d75bf48ea1e3665987b1
mimisosorara <igi712.mgj@gmail.com>
2024-05-16 22:09:55 +0700

    Pertemuan 11

---
+++ Pertemuan11/src/main/java/Refactored/Passenger.java
@@ -0,0 +1,16 @@
+package Refactored;
+
+public class Passenger {
+    private String name;
+    private boolean vip;
+    public Passenger(String name, boolean vip) {
+        this.name = name;
+        this.vip = vip;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isVip() {
+        return vip;
+    }
+}
